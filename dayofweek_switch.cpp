ae729a5359c6cd8e9e8f48e6869a5362e2c076cc
um26 <utkarsh7023340530@gmail.com>
2024-01-02 23:25:46 +0530

    added code

--- dayofweek_switch.cpp
+++ dayofweek_switch.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
 
+using namespace std;
+
+string day_of_the_week(int day_num){
+    string day_name
+    switch(day_num){
+        case(0):
+            day_name= "Sunday";
+            break;
+        case(1):
+            day_name= "Monday";
+            break;
+        case(2):
+            day_name= "Tuesday";
+            break;
+        case(3):
+            day_name= "Wednesday";
+            break;
+        case(4):
+            day_name= "Thursday";
+            break;
+        case(5):
+            day_name= "Friday";
+            break;
+        case(6):
+            day_name= "Satuday";
+            break;
+        default:
+            day_name="Invalid Day Number"
+            
+    }
+} 
+
+int main(){
+    cin>> day_num;
+    cout<< day_of_the_week(day_num)\n;
+}
