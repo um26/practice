dc06ded617c68f28e70eb85ab185f87d804503b7
um26 <utkarsh7023340530@gmail.com>
2024-01-03 10:56:31 +0530

    added code

--- books.cpp
+++ books.cpp
@@ -1,1 +1,24 @@
 
+
+class Book{
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
+int main(){
+    Book book1;
+    book1.title="Harry Potter";
+    book1.author="JK Rowling";
+    book1.pages=500;
+    
+    Book book2;
+    book2.title="Lord of the Rings";
+    book2.author="Tolkien";
+    book2.pages=700;
+
+    cout<<book1.author<<endl;
+    cout<<book2.title<<endl;
+
+}
